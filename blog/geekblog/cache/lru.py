18efaf326180c727a5c465175dab79000220067d
Wenyuan Wu <wenyuanwu@sohu-inc.com>
2014-08-22 10:50:49 +0800

    add lru cache and update mobile templates.

---
+++ blog/geekblog/cache/lru.py
@@ -0,0 +1,194 @@
+# -*- coding: UTF-8 -*-
+"""
+https://github.com/stucchio/Python-LRU-cache/blob/master/lru/__init__.py
+"""
+from time import time
+from collections import OrderedDict
+
+
+def lru_cache_function(max_size=1024, expiration=15*60):
+    """
+    >>> @lru_cache_function(3, 1)
+    ... def f(x):
+    ...    print "Calling f(" + str(x) + ")"
+    ...    return x
+    >>> f(3)
+    Calling f(3)
+    3
+    >>> f(3)
+    3
+    """
+    def wrapper(func):
+        return LRUCachedFunction(func, LRUCacheDict(max_size, expiration))
+    return wrapper
+
+
+class LRUCacheDict(object):
+    """ A dictionary-like object, supporting LRU caching semantics.
+
+    >>> d = LRUCacheDict(max_size=3, expiration=3)
+    >>> d['foo'] = 'bar'
+    >>> d['foo']
+    'bar'
+    >>> import time
+    >>> time.sleep(4) # 4 seconds > 3 second cache expiry of d
+    >>> d['foo']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'foo'
+    >>> d['a'] = 'A'
+    >>> d['b'] = 'B'
+    >>> d['c'] = 'C'
+    >>> d['d'] = 'D'
+    >>> d['a'] # Should return value error, since we exceeded the max cache size
+    Traceback (most recent call last):
+        ...
+    KeyError: 'a'
+    """
+    def __init__(self, max_size=1024, expiration=15*60):
+        self.max_size = max_size
+        self.expiration = expiration
+
+        self.__values = {}
+        self.__expire_times = OrderedDict()
+        self.__access_times = OrderedDict()
+
+    def size(self):
+        return len(self.__values)
+
+    def clear(self):
+        """
+        Clears the dict.
+
+        >>> d = LRUCacheDict(max_size=3, expiration=1)
+        >>> d['foo'] = 'bar'
+        >>> d['foo']
+        'bar'
+        >>> d.clear()
+        >>> d['foo']
+        Traceback (most recent call last):
+        ...
+        KeyError: 'foo'
+        """
+        self.__values.clear()
+        self.__expire_times.clear()
+        self.__access_times.clear()
+
+    def has_key(self, key):
+        """
+        This method should almost NEVER be used. The reason is that between the time
+        has_key is called, and the key is accessed, the key might vanish.
+
+        You should ALWAYS use a try: ... except KeyError: ... block.
+
+        >>> d = LRUCacheDict(max_size=3, expiration=1)
+        >>> d['foo'] = 'bar'
+        >>> d['foo']
+        'bar'
+        >>> import time
+        >>> if d.has_key('foo'):
+        ...    time.sleep(2)    # Oops, the key 'foo' is gone!
+        ...    d['foo']
+        Traceback (most recent call last):
+        ...
+        KeyError: 'foo'
+        """
+        return self.__values.has_key(key)
+
+    def __setitem__(self, key, value):
+        t = int(time())
+        self.__delete__(key)
+        self.__values[key] = value
+        self.__access_times[key] = t
+        self.__expire_times[key] = t + self.expiration
+        self.cleanup()
+
+    def __getitem__(self, key):
+        t = int(time())
+        value = self.__values[key]
+        if self.__expire_times[key] < t:
+            del self.__values[key]
+            del self.__expire_times[key]
+            del self.__access_times[key]
+            raise KeyError
+        self.__access_times[key] = t
+        return value
+
+    def __delete__(self, key):
+        if self.__values.has_key(key):
+            del self.__values[key]
+            del self.__expire_times[key]
+            del self.__access_times[key]
+
+    def cleanup(self):
+        if self.expiration is None:
+            return None
+        t = int(time())
+        # delete expired
+        for k in self.__expire_times.iterkeys():
+            if self.__expire_times[k] < t:
+                self.__delete__(k)
+            else:
+                break
+
+        # if we have more than self.max_size items, delete the oldest
+        while len(self.__values) > self.max_size:
+            for k in self.__access_times.iterkeys():
+                self.__delete__(k)
+                break
+
+
+class LRUCachedFunction(object):
+    """
+    A memoized function, backed by an LRU cache.
+
+    >>> def f(x):
+    ...    print "Calling f(" + str(x) + ")"
+    ...    return x
+    >>> f = LRUCachedFunction(f, LRUCacheDict(max_size=3, expiration=3) )
+    >>> f(3)
+    Calling f(3)
+    3
+    >>> f(3)
+    3
+    >>> import time
+    >>> time.sleep(4) #Cache should now be empty, since expiration time is 3.
+    >>> f(3)
+    Calling f(3)
+    3
+    >>> f(4)
+    Calling f(4)
+    4
+    >>> f(5)
+    Calling f(5)
+    5
+    >>> f(3) #Still in cache, so no print statement. At this point, 4 is the least recently used.
+    3
+    >>> f(6)
+    Calling f(6)
+    6
+    >>> f(4)    # No longer in cache - 4 is the least recently used, and there are at least 3 others items in cache [3,4,5,6].
+    Calling f(4)
+    4
+
+    """
+    def __init__(self, function, cache=None):
+        if cache:
+            self.cache = cache
+        else:
+            self.cache = LRUCacheDict()
+        self.function = function
+        self.__name__ = self.function.__name__
+
+    def __call__(self, *args, **kwargs):
+        key = repr((args, kwargs))
+        try:
+            return self.cache[key]
+        except KeyError:
+            value = self.function(*args, **kwargs)
+            self.cache[key] = value
+            return value
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
