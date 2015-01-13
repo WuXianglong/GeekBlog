be385642a232522d8f51452b0dfda7dceb0ae1d9
Xianglong Wu <wuxianglong098@gmail.com>
2015-01-13 19:07:10 +0800

    update nginx conf & remove ip in ALLOW_HOSTS

--- blog/geekblog/geek_blog/settings.py
+++ blog/geekblog/geek_blog/settings.py
@@ -89,7 +89,7 @@ PUBLISH_DATE_FORMAT = '%Y-%m-%d'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.xianglong.me', '115.28.143.213']
+ALLOWED_HOSTS = ['.xianglong.me']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
